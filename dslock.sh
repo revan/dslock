6c2832f504d81f454f00d9370c96f92bec55c126
Revan Sopher <rsopher@gmail.com>
2014-10-19 22:07:44 -0400

    YOU DIED

--- dslock.sh
+++ dslock.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export DSLOCK="/tmp/dslock.png"
 
 cd "$(dirname -- "$0")"
-scrot -z /tmp/dslock.png && python dsify.py /tmp/dslock.png && i3lock -i /tmp/dslock.png
+scrot -z $DSLOCK && python dsify.py $DSLOCK && i3lock -i $DSLOCK; rm -f $DSLOCK
